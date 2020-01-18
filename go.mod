ad5bbcf452d564d79cb24c55231f60dc0aac5661
Sohxm Kulkxrni <45848414+ThatXuxe@users.noreply.github.com>
2020-01-18 20:36:35 +0530

    GCI-xuxe-revised
    Revised code

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ThatXuxe/GCI-OpenMRS-GO
 go 1.13
 
 require (
-	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/gin-gonic/gin v1.5.0
 	github.com/gorilla/mux v1.7.3
 )
--- main.go
+++ main.go
@@ -1,47 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
-// Message object
-type msg struct {
-	Message string
-}
-
-// / endpoint
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	user := &msg{Message: "Hello, World!"}
-	b, err := json.Marshal(user)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Fprintf(w, string(b))
+// /hello endpoint
+func homeLink(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello, World!",
+	})
 }
 
 // /greet/{name} endpoint
-func nameLink(w http.ResponseWriter, r *http.Request) {
-	name := "Hello " + mux.Vars(r)["name"]
-	user := &msg{Message: name}
-	b, err := json.Marshal(user)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Fprintf(w, string(b))
+func nameLink(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello, " + c.Param("name"),
+	})
 }
 
 func main() {
 	// init router
-	router := mux.NewRouter().StrictSlash(true)
+	router := gin.Default()
 	// register endpoints
-	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/greet/{name}", nameLink).Methods("GET")
+	router.GET("/hello", homeLink)
+	router.GET("/greet/:name", nameLink)
 	// print server status
-	fmt.Println(http.ListenAndServe(":8088", router))
+	router.Run(":8088")
 }
